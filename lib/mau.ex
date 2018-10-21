08dab6082c06a7b3b32f26bdbc489a27b1472f23
Werner Hofstra <w.hofstra@catawiki.nl>
2018-10-21 22:37:54 +0200

    Remove existing bp

--- lib/mau.ex
+++ lib/mau.ex
@@ -1,18 +1,2 @@
 defmodule Mau do
-  @moduledoc """
-  Documentation for Mau.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mau.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
