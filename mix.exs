a5287eb51fd7839b7d70e9b2ba71137e4fada463
Werner Hofstra <w.hofstra@catawiki.nl>
2018-10-21 22:36:31 +0200

    Add Deck dependency

--- mix.exs
+++ mix.exs
@@ -21,8 +21,7 @@ defmodule Mau.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:deck, git: "https://github.com/wgfm/deck.git"},
     ]
   end
 end
